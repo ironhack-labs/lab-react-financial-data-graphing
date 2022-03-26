560918cd2ca77a55b1c59d3143d053f72abc8f2c
Bruno Mortella <bmortella@gmail.com>
2022-03-26 15:28:47 -0300

    Iteration 4

--- src/components/CoinChart.js
+++ src/components/CoinChart.js
@@ -6,17 +6,27 @@ import Chart from "chart.js/auto";
 function CoinChart() {
   const [dates, setDates] = useState([]);
   const [values, setValues] = useState([]);
+  const [currency, setCurrency] = useState("USD");
   const [chart, setChart] = useState();
 
-  const [startDate, setStartDate] = useState();
-  const [endDate, setEndDate] = useState();
+  let today = new Date();
+  today.setDate(today.getDate() - 1);
+  const [endDate, setEndDate] = useState(today.toISOString().split("T")[0]);
+
+  today.setDate(today.getDate() - 30);
+  const [startDate, setStartDate] = useState(today.toISOString().split("T")[0]);
 
   useEffect(() => {
     axios
-      .get("http://api.coindesk.com/v1/bpi/historical/close.json")
+      .get(
+        `http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`
+      )
       .then((response) => {
         setDates(Object.keys(response.data.bpi));
         setValues(Object.values(response.data.bpi));
+
+        document.getElementById("startDate").value = startDate;
+        document.getElementById("endDate").value = endDate;
       })
       .catch((error) => console.error(error));
   }, []);
@@ -54,7 +64,7 @@ function CoinChart() {
     if (startDate && endDate) {
       axios
         .get(
-          `http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`
+          `http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`
         )
         .then((response) => {
           setDates(Object.keys(response.data.bpi));
@@ -62,7 +72,7 @@ function CoinChart() {
         })
         .catch((error) => console.error(error));
     }
-  }, [startDate, endDate]);
+  }, [startDate, endDate, currency]);
 
   function handleStartChange(event) {
     setStartDate(event.target.value);
@@ -72,11 +82,20 @@ function CoinChart() {
     setEndDate(event.target.value);
   }
 
+  function handleCurrency(event) {
+    setCurrency(event.target.options[event.target.selectedIndex].text);
+  }
+
   return (
     <div>
       <div>
         <input type="date" id="startDate" onChange={handleStartChange}></input>
         <input type="date" id="endDate" onChange={handleEndChange}></input>
+        <select id="currency" onChange={handleCurrency} defaultValue={currency}>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="BRL">BRL</option>
+        </select>
       </div>
       <div style={{ height: "400px" }}>
         <canvas id="myChart" width="400" height="400"></canvas>
