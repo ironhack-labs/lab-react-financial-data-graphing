563cc25616de5230bd564a3c46d897872b6c9733
Bruno Mortella <bmortella@gmail.com>
2022-03-26 14:44:15 -0300

    Iteration 2

--- src/components/CoinChart.js
+++ src/components/CoinChart.js
@@ -1,13 +1,57 @@
+import { useEffect, useState } from "react";
+
 import axios from "axios";
+import Chart from "chart.js/auto";
 
 function CoinChart() {
-  axios
-    .get("http://api.coindesk.com/v1/bpi/historical/close.json")
-    .then((response) => {
-      console.log(response);
+  const [dates, setDates] = useState([]);
+  const [values, setValues] = useState([]);
+  const [chart, setChart] = useState();
+
+  useEffect(() => {
+    axios
+      .get("http://api.coindesk.com/v1/bpi/historical/close.json")
+      .then((response) => {
+        setDates(Object.keys(response.data.bpi));
+        setValues(Object.values(response.data.bpi));
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  useEffect(() => {
+    const ctx = document.getElementById("myChart");
+
+    if (chart) {
+      chart.destroy();
+    }
+
+    const myChart = new Chart(ctx, {
+      type: "line",
+      responsive: true,
+      maintainAspectRatio: false,
+      data: {
+        labels: dates,
+        datasets: [
+          {
+            label: "Variação de preço de BitCoin",
+            data: values,
+            fill: true,
+            borderColor: "rgb(75, 192, 192)",
+            backgroundColor: "rgb(75, 192, 192)",
+            tension: 0,
+          },
+        ],
+      },
     });
 
-  return <div></div>;
+    setChart(myChart);
+  }, [dates, values]);
+
+  return (
+    <div style={{ height: "400px" }}>
+      <canvas id="myChart" width="400" height="400"></canvas>
+    </div>
+  );
 }
 
 export default CoinChart;
