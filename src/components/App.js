feb12fb15359a1f1457035dcaddac7a767bb6a06
Nathalia Maia <nathaliamaiape@gmail.com>
2022-03-27 22:45:13 -0300

    finished all iterations

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,15 @@
 
 import MyChart from './MyChart'
-import "bootstrap/dist/css/bootstrap.css";
+
 
 function App() {
 
   return (
-     
-    <div className="container mt-5" >
+      <div className="container mt-5" >
       
-     <MyChart/>
+      <MyChart/>
        
-    </div>
+      </div>
   );
 }
 
--- src/components/MyChart.js
+++ src/components/MyChart.js
@@ -7,31 +7,56 @@ function MyChart() {
     const [pricesVariation, setPricesVariation] = useState([]);
     const [chart, setChart] = useState();
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await axios.get(
-                    "http://api.coindesk.com/v1/bpi/historical/close.json"
-                );
-                const entries = Object.entries(response.data["bpi"]);
+    const[start, setStart] = useState();
+    const[end, setEnd] = useState();
+    const[currency, setCurrency] = useState('USD');
 
-                setDates(entries.map((currentArr) => currentArr[0]));
-                setPricesVariation(entries.map((currentArr) => currentArr[1]));
-                console.log(entries);
-            } catch (err) {
-                console.error(err);
-            }
+    const[values, setValues] = useState();
+
+    function handleFilterStart(event) {
+      setStart(event.target.value)
+    };
+    
+    function handleFilterEnd(event) {
+      setEnd(event.target.value)
+    };
+
+    function handleCurrency(event) {
+      setCurrency(event.target.value);
+    }
+
+    function handleValues(event) {
+      setValues(event.target.value);
+    }
+
+    useEffect(() => {
+      if (start && end) {
+        async function fetchData() {
+          try {
+            const response = await axios.get(
+              `http://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}&currency=${currency}`
+            );
+
+            const entries = Object.entries(response.data["bpi"]);
+            setDates(entries.map((currentArr) => currentArr[0]));
+            setPricesVariation(entries.map((currentArr) => currentArr[1]));
+            
+          } catch (err) {
+            console.error(err);
+          }
         }
         fetchData();
-    }, []);
+      } 
+
+    }, [start, end, currency]);
 
     useEffect(() => {
+    
         const ctx = document.getElementById("myChart");
 
         if (chart) {
             chart.destroy();
         }
-
         const myChart = new Chart(ctx, {
             type: "line",
             options: { responsive: true, maintainAspectRatio: false },
@@ -52,9 +77,44 @@ function MyChart() {
     }, [dates, pricesVariation]);
 
     return (
-        <div style={{ position: "relative", height: "600px" }}>
-            <canvas id="myChart" width="400" height="400"></canvas>
+      <div>
+        <div>
+          <h2 className="mb-3">Filters</h2>
+          <div>
+            <label>
+              <strong>From: </strong>
+            </label>
+            <input
+              type="date"
+              style={{ marginRight: "1rem" }}
+              id="from"
+              onChange={handleFilterStart}
+            />
+            <label>
+              <strong>To:</strong>
+            </label>
+            <input type="date" id="to" onChange={handleFilterEnd} />
+          </div>
+          <div>
+            <label>
+              <strong>Currency:</strong>
+            </label>
+            <select value={currency} className="mt-3" onChange={handleCurrency}>
+              <option value="EUR">EUR</option>
+              <option value="USD">USD</option>
+            </select>
+          </div>
+          <div>
+            <h2>Values</h2>
+            <p>max: {Math.max(...pricesVariation)} {currency}</p>
+            <p>min: {Math.min(...pricesVariation)} {currency}</p>
+          </div>
         </div>
+        <div style={{ position: "relative", height: "600px" }}>
+          <canvas id="myChart" width="400" height="400"></canvas>
+        </div>
+      </div>
     );
 }
-export default MyChart;
+export default MyChart; 
+
